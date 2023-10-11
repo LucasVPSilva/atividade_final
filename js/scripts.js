57b73f262d71aeac8c195ccae5e9dbd77c511358
Lucas Pires Silva <118729077+LucasVPSilva@users.noreply.github.com>
2023-10-11 13:53:06 -0300

    incluido animacao ao clicar no card

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ const prevPage = document.getElementById('prevPage');
 const nextPage = document.getElementById('nextPage');
 
 const infoPage = document.getElementById('infoPage');
+const infoCharacter = document.getElementById('infoCharacter');
+const infoLocations = document.getElementById('infoLocations');
+const infoEpisodes = document.getElementById('infoEpisodes');
+
 
 let currentPage = 1
 let totalPages = 0;
@@ -28,11 +32,21 @@ async function loadCharacters(name = '', page) {
         prevPage.disabled = true;
         nextPage.disabled = true;
 
+        // Localizações:
+
+        const responseLocation = await api.get('/location');
+        console.log(responseLocation.data)
+
+
+        // Episodios:
+
+        const responseEpisode = await api.get('/episode')
+        console.log(responseEpisode.data)
 
 
         characterList.innerHTML = ``
 
-        response.data.results.forEach(character => {
+        response.data.results.forEach((character, index) => {
             const characterDivBoot = document.createElement('div');
             characterDivBoot.classList.add('col-12', 'col-sm-6', 'col-md-4', 'col-lg-3', 'card-group');
 
@@ -41,12 +55,13 @@ async function loadCharacters(name = '', page) {
 
 
             characterCard.innerHTML = `
-                <img src="${character.image}" class = "card-img-top"/>
+              <div data-bs-toggle="modal" data-bs-target="#meuModal-${index}" data-character='${JSON.stringify(character)}'>
+                <img src="${character.image}" class = "card-img-top p-2 rounded img-rounded"/>
                 
                 <div class="p-2 card-body">
                   
 
-                    <h5 class="card-title text-light p-0 m-0 font-weight-bold mb-0"> ${character.name}</h5>
+                    <h5 class="card-title text-light font-weight-bold mb-1 fs-3"> ${character.name}</h5>
                 
                                     
                    <div class="card-status mb-2">
@@ -68,7 +83,7 @@ async function loadCharacters(name = '', page) {
                     
                     <small class="text-white-50 font-weight-bold"
                           >Última localização conhecida</small>
-                    <p class="fs-1 mb-0 pb-0 fs-6 text-light mb-2">${character.location.name}</p>
+                    <p class="mb-0 pb-0 text-light mb-2">${character.location.name}</p>
                             <small class="text-white-50 font-weight-bold"
                             >Visto a última vez em:</small
                          >
@@ -76,17 +91,64 @@ async function loadCharacters(name = '', page) {
                              Nome do capítulo
                          </p>
 
+
+
+                         
+
                 </div>
-                   
-                   
+
+
+                <!--modal-->
+                <div class="modal" id="meuModal-${index}">
+                        <div class="modal-dialog ">
+                        <div class="modal-content bg-dark">
+                            <div class="modal-header justify-content-center">
+                            <img src="${character.image}" class = "img-fluid animated-image rounded img-rounded"/>
+                          
+                            </div>
+                            <div class="modal-body">
+                            <div class="">
+                            <h5 class="card-title text-light font-weight-bold mb-3 fs-3"> ${character.name}</h5>
+                                <small class="text-white-50 font-weight-bold"
+                                >Status</small>
+                                <p class="card-text text-light fw-semibold"> ${character.status == "Dead"
+                    ? "Morto"
+                    : character.status == "Alive"
+                        ? "Vivo"
+                        : "Desconhecido"}
+                                </p>
+                                <small class="text-white-50 font-weight-bold"
+                                    >Raça</small>
+                                <p class="mb-0 pb-0 text-light mb-1">${character.species == "Human" ? "Humano" : character.species}</p>
+                                </p>
+                                <small class="text-white-50 font-weight-bold"
+                                    >Última localização conhecida</small>
+                                <p class="mb-0 pb-0 text-light mb-2">${character.location.name}</p>    
+
+                            </div>
+                            </div>
+                            <div class="modal-footer">
+                                 <button class="btn btn-secondary " data-bs-dismiss="modal">Fechar</button>
+                            </div>
+                        </div>
+                        </div>
+              </div>
+   
             `;
+
+
 
             characterDivBoot.appendChild(characterCard)
             characterList.appendChild(characterDivBoot)
 
 
 
-            infoPage.innerHTML = `<p> Página: ${currentPage}/${totalPages} </p>`
+            infoPage.innerHTML = `<p class="text-success "> Página:  <span class="text-light">${currentPage}/${totalPages}</span> </p>`
+            infoCharacter.innerHTML = `<p class="text-success"> Personagens: <span class="text-light">${response.data.info.count}<span> </p>`
+            infoLocations.innerHTML = `<p class="text-success"> Localizações: <span class="text-light"> ${responseLocation.data.info.count}</span> </p>`
+            infoEpisodes.innerHTML = `<p class="text-success"> Episódios: <span class="text-light"> ${responseEpisode.data.info.count}</span> </p>`
+
+
 
             prevPage.disabled = !response.data.info.prev
             nextPage.disabled = !response.data.info.next
@@ -135,29 +197,3 @@ nextPage.addEventListener('click', () => {
 
 
 
-/*        
-    const characterCard = document.createElement('div');
-            characterCard.classList.add('characters-card');
-
-
-            characterCard.innerHTML = `
-            <img src="${character.image}" class = "characters-image"/>
-                    <h2> ${character.name}</h2>
-                    <div class="cardStatus">
-                        <div class='${character.status == 'Dead'
-                    ? 'dead' : character.status == 'Alive' ? 'alive'
-                        : 'unknown'}'>
-                         </div> 
-                         ${character.status == 'Dead'
-                    ? 'Morto' : character.status == 'Alive' ? 'Vivo'
-                        : 'Desconhecido'}&nbsp - &nbsp${character.species ==
-                            'Human' ? "Humano" : character.species}
-                   </div>
-                   <p>Última localização conhecida:</p>
-                   <h4>${character.location.name}</h4> 
-            `
-
-            characterList.appendChild(characterCard)
-
-
-*/
